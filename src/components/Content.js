59722a78bd429b09a218b199c3273aab4e9418a3
Bradley Service <bradleykservice@gmail.com>
2021-01-12 13:35:29 -0600

    edits

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,9 +7,10 @@ export default function Content() {
         <div className='content'>
             <div className='about'>
                     <img src={photo} className='profile-img' style={{height: '300px', width: '300px', borderRadius: '50%', border: '2px solid white'}}></img>
-                <div style={{display: 'flex', flexDirection: 'column', width: '40vw', border: '2px solid white', width: '40%', padding: '30px'}}>
-                    {/* <h6 style={{display: 'flex', justifyContent: 'center', fontSize: '20px', margin: '7px'}}>About Me:</h6><br/> */}
-                    <div>
+                    {/* <div style={{display: 'flex', alignItems: 'flex-start', height: '240px', fontFamily: 'Major Mono Display, monospace', fontSize: '20px'}}>About</div> */}
+                <div style={{display: 'flex', flexDirection: 'column', width: '40vw', border: '2px solid white', width: '40%', padding: '30px', marginTop: '60px'}}>
+                    <div style={{display: 'flex', alignItems: 'flex-start', fontFamily: 'Major Mono Display, monospace', fontSize: '28px', fontWeight: 'bold', color: '#283845'}}>About me</div><br/>
+                    <div style={{color: '#283845'}}>
                         As a former bartender turned web developer, I bring more than just technical skills to the table.
                         Spending over a decade in the service industry taught me valuable skills such as 
                         communication, empathy, and teamwork.
@@ -17,10 +18,11 @@ export default function Content() {
                         needed to be a full-stack developer. I have experience building full stack applications on my own, 
                         but prefer the social setting of a group in an agile work environment. 
                     </div>
-                    <div style={{marginTop: '20px'}}>bradleykservice@gmail.com</div>
+                    <div style={{marginTop: '20px', color: '#283845'}}>bradleykservice@gmail.com</div>
                 </div>
             </div>
             <div style={{display: 'flex', flexDirection: 'column', marginTop: '30px'}}>
+                <div style={{display: 'flex', justifyContent: 'center', fontFamily: 'Major Mono Display, monospace', color: 'white', fontSize: '28px', fontWeight: 'bolder', margin: '18px'}}>skills</div>
                 <div style={{display: 'flex', justifyContent: 'space-evenly', width: '50vw', margin: '5px auto'}}>
                     <img src='css.svg' alt='icon' />
                     <img src='express.svg' alt='icon' />
@@ -52,6 +54,7 @@ export default function Content() {
                     <div>SASS</div>
                 </div>
             </div>
+            <div style={{backgroundColor: '#283845', height: '15vh', width: '100%', border: '2px solid white'}}></div>
         </div>
     )
 }
